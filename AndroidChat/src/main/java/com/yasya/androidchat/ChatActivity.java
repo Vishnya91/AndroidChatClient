24f8c8c1cad0a416e81765996da16cd3bb54d148
Vishnya91 <yasyaklio@gmail.com>
2013-10-30 12:19:19 +0200

    added handler for listview update

--- AndroidChat/src/main/java/com/yasya/androidchat/ChatActivity.java
+++ AndroidChat/src/main/java/com/yasya/androidchat/ChatActivity.java
@@ -8,10 +8,12 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
+import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -21,6 +23,7 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.os.Message;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -93,13 +96,19 @@ public class ChatActivity extends Activity {
     private void checkConnectivity() {
         if (!connected && isNetworkAvailable()) {
             getLogin();
+            this.setTitle("Android Chat - disconnected");
+            this.setTitleColor(Color.RED);
         } else if (!isNetworkAvailable()) {
             btnSend.setEnabled(false);
             txtEdit.setEnabled(false);
             Toast.makeText(this, "Network isn't available", Toast.LENGTH_LONG).show();
+            this.setTitle("Android Chat - disconnected");
+            this.setTitleColor(Color.RED);
         } else if (connected) {
             Toast.makeText(this, "Connected!", Toast.LENGTH_LONG).show();
             initFragment();
+            this.setTitle("Android Chat - connected");
+            this.setTitleColor(Color.GREEN);
 
         }
         //todo check for internet connection and connected = true
@@ -231,15 +240,12 @@ public class ChatActivity extends Activity {
             if (msg == null) {
                 break;
             } else {
-                /*
-                 * Message message = Message.obtain(); // Creates an new Message
-				 * // instance message.obj = msg; // Put the string into
-				 * Message, into "obj" // field. message.setTarget(handler); //
-				 * Set the Handler message.sendToTarget();
-				 */
                 receiver.receiveString(msg);
-                msgList.add(receiver.UIMessage());
-                msgView.setAdapter(msgList);
+
+                Message message = Message.obtain();
+                message.obj = receiver.UIMessage();
+                message.setTarget(handler);
+                message.sendToTarget();
             }
         }
     }
@@ -259,14 +265,20 @@ public class ChatActivity extends Activity {
             Toast.makeText(getBaseContext(), "Press once again to exit!", Toast.LENGTH_SHORT).show();
         back_pressed = System.currentTimeMillis();
     }
-    /*
-     * private Handler handler = new Handler() {
-     * @Override // When there is message, execute this method public void
-	 * handleMessage(Message msg) { super.handleMessage(msg); String message =
-	 * (String) msg.obj; // Update UI msgList.add(message);
-	 * msgView.setAdapter(msgList);
-	 * msgView.smoothScrollToPosition(msgList.getCount() - 1); } };
-	 */
+
+    private Handler handler = new Handler() {
+        @Override
+        // When there is message, execute this method
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            String message = (String) msg.obj;
+            // Update UI
+            msgList.add(message);
+            msgView.setAdapter(msgList);
+            msgView.smoothScrollToPosition(msgList.getCount() - 1);
+        }
+    };
+
 
     public class NetworkTask extends AsyncTask<Void, Void, Void> {
         @Override
